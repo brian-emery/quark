c268850f4b07d9037fe3efc530cc7fa928467a37
Stephen Fox <sfox@largo.com>
2017-09-25 17:23:56 -0400

    Add logging and more informative output from run.py

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from create_blog import app
 from ConfigParser import SafeConfigParser
+import tornado.options
+tornado.options.parse_command_line()
 
 parser = SafeConfigParser()
 parser.read('config.txt')
@@ -11,4 +13,9 @@ lport = parser.get('config', 'PORT')
 
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(lport)
-IOLoop.instance().start()
+print('Server running on http://localhost:' + lport)
+try:
+    IOLoop.instance().start()
+except:
+    print('\nExiting...')
+    IOLoop.instance().stop()
